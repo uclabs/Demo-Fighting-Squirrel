361f015b27513886b917813f594436931a583e38
xiongdan <xiongdan506@163.com>
2012-12-28 10:33:53 +0800

    添加场景切换

--- src/js/view/Scene.js
+++ src/js/view/Scene.js
@@ -22,49 +22,62 @@ elf.define('FS::View::Scene', [
             ctor: function () {
                 this._super();
                 cc.associateWithNative( this, cc.Layer );
+            },
+            replaceScene: function (Sprite, type, time) {
+                var scene = cc.Scene.create(),
+                    transition = null;
+                scene.addChild(Sprite);
+                time = time || 0;
+                switch (type) {
+                    case 'Fade' :      //淡出前一场景
+                        transition = cc.TransitionFade.create(time, scene);
+                        break;
+                    case 'JumpZoom':   //跳跃式替换，场景缩小，再加载进来
+                        transition = cc.TransitionJumpZoom.create(time, scene);
+                        break;
+                    case 'ShrinkGrow': //交叉着替换场景
+                        transition = cc.TransitionShrinkGrow.create(time,scene);
+                        break;
+                    case 'RotoZoom':   //转换角度替换
+                        transition = cc.TransitionRotoZoom.create(time,scene);
+                        break;
+                    case 'MoveInL':     //从左切入
+                        transition = cc.TransitionMoveInL.create(time, scene);
+                        break;
+                    case 'MoveInR':     //从右切入
+                        transition = cc.TransitionMoveInR.create(time, scene);
+                        break;
+                }
+                cc.Director.getInstance().replaceScene(transition);
             }
         }),
-        Scene;
-    Sprite.create = function () {
-        var sg = new Sprite();
-        if (sg) {
-            return sg;
-        }
-        return null;
-    };
-    Sprite.scene = function () {
-        var scene = cc.Scene.create();
-        var layer = Sprite.create();
-        scene.addChild(layer);
-        return scene;
-    };   
-    Scene = Class.extend({
-        type: 'Scene',
-        ctor: function (opts) {
-            this.mix(eventMixin, messageMixin, elementMixin, stateMixin);
-            this.config(opts);
-            this.listenController(opts.uuid, this.invoke.bind(this));
+        Scene = Class.extend({
+            type: 'Scene',
+            ctor: function (opts) {
+                this.mix(eventMixin, messageMixin, elementMixin, stateMixin);
+                this.config(opts);
+                this.listenController(opts.uuid, this.invoke.bind(this));
 
-            // 派发场景创建消息
-            this.sendMessage('Scene:create', [this]);
-            
-            // 创建 Cocos2d 对象
-            this.sprite = new Sprite.scene ();
-           
-        },
-        mix: function () {
-            _.extend.apply(_, concat.apply([true, this], arguments));
-        },
-        stateHandler: function () {
-        },
-        // 改变回合方
-        changeSide: function(side) {
-            log('view:' + this.type + ':' + this.uuid, 'side', side);
-        },
-        addChild: function(child, zOrder, tag) {
-            this.sprite.addChild(child, zOrder || 1, tag || 1);
-        }
-    });
+                // 派发场景创建消息
+                this.sendMessage('Scene:create', [this]);
+                
+                // 创建 Cocos2d 对象
+                this.sprite = new Sprite();
+               
+            },
+            mix: function () {
+                _.extend.apply(_, concat.apply([true, this], arguments));
+            },
+            stateHandler: function () {
+            },
+            // 改变回合方
+            changeSide: function(side) {
+                log('view:' + this.type + ':' + this.uuid, 'side', side);
+            },
+            addChild: function(child, zOrder, tag) {
+                this.sprite.addChild(child, zOrder || 1, tag || 1);
+            }
+        });
 
     Scene.type = 'Scene';
 
